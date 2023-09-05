493da71786c08d7790f9b79b6d6f7c84612cc80b
Jordan <jordynwinnie@gmail.com>
2023-09-05 22:08:21 +0800

    Added Tailwind and Landing page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    function get_link() {
+        const CLIENT_ID = '3fd670a228914c1796907aab9d0a965f'
+        const REDIRECT_URL = window.location + 'callback'
+        const SCOPES = "user-top-read user-read-email user-read-private"
+
+        const finalUrl = new URL(
+            "https://accounts.spotify.com/authorize"
+        )
+
+        finalUrl.searchParams.append("response_type", "code")
+        finalUrl.searchParams.append("client_id", CLIENT_ID)
+        finalUrl.searchParams.append("redirect_uri", REDIRECT_URL)
+        finalUrl.searchParams.append("scope", SCOPES)
+        
+        return finalUrl.toString()
+    }
+</script>
+
+<h1 class="text-center">Welcome to Musical Mosaic</h1>
+<div class="items-center justify-center flex">
+    <button on:click={() => { 
+       
+        window.location.replace(get_link())
+        
+        } } class="bg-green-500 m-4 py-8 rounded-full text-4xl px-20">
+        Login to Spotify
+    </button>
+</div>
