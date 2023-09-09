1a5a7419caa670e7f88d5bd02ee198227025d9e0
Jordan <jordynwinnie@gmail.com>
2023-09-09 15:07:47 +0800

    Added Download image function

--- frontend/src/routes/results/+page.svelte
+++ frontend/src/routes/results/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from 'svelte';
+    import html2canvas from 'html2canvas';
     let songs = []
     let username = ""
     onMount(async () => {
@@ -33,20 +34,51 @@
 
         return song_json.album.images[0].url
     }
+
+    function render_image() {
+        const mosaic = document.getElementById('mosaic')
+        html2canvas(mosaic, {
+            useCORS: true,
+            scrollX: 0,
+            scrollY: 0,
+            scale: 4
+        }).then(
+            function(canvas) {
+                const uri = canvas.toDataURL("image/jpeg")
+
+                const link = document.createElement('a')
+
+                if (typeof link.download === 'string') {
+                    link.href = uri;
+                    link.download = 'Mosaic.jpeg'
+
+                    //Firefox requires the link to be in the body
+                    document.body.appendChild(link);
+
+                    link.click()
+
+                    document.body.removeChild(link);
+                } else {
+                    window.open(uri)
+                }
+            }
+        )
+    }
 </script>
 
 <svelte:head>
     <style>
-
         @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Ubuntu&display=swap');
     </style>
 </svelte:head>
 {#if username !== ''}
-<div class="flex flex-col justify-center place-items-center ">
-    <div class="w-80 text-center bg-blue-200 font-sans">
+<div class="flex flex-col justify-center place-items-center" >
+    <div class="w-80 text-center bg-blue-200 font-sans" id="mosaic">
+        <div class="h-6 bg-blue-500 flex justify-center items-center px-2">
+            <p class="text-lg font-bebas">{username}'s Musical Mosaic</p>
+         </div>
         
-        
-        <div class="grid grid-cols-5">
+        <div class="grid grid-cols-5 p-1">
             {#each songs as song, idx}
                  {#if idx === 6}
                  <img class="row-span-3 col-span-3 object-fill" src={songs[idx].album.images[0].url} alt='Album Art for {songs[idx].name}'>
@@ -58,19 +90,16 @@
             {/each}
          </div>
 
-         <div class="h-8 bg-blue-500">
-            <div class="justify-between flex items-center h-full px-2 py-1.5">
-                <img class="h-full" src='Spotify_Logo_RGB_Black.png'>
-                <div class="flex flex-col font-bebas text-right">
-                    <p class="text-xs">{username}'s Musical Mosaic</p>
-                    <p class="text-xs underline">musical-mosaic.pages.dev</p>
-                </div>
-                
-            </div>
-            
+         <div class="h-6 bg-blue-500 flex justify-between items-center px-2">
+            <img class="h-full py-1" src='Spotify_Logo_RGB_Black.png'>
+            <p class="text-xs font-bebas">{window.location.host}</p>
          </div>
     </div>
+
+    <button class="rounded bg-slate-200 p-4 m-2" on:click={render_image}>Download Image</button>
 </div>
+
+
 {/if}
 
 
