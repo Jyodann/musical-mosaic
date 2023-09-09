98a2cf759879a67a96dfbadb1b6e2f7fd9dcbb7e
Jordan <jordynwinnie@gmail.com>
2023-09-10 00:37:38 +0800

    Increased Scale for better legibility

--- frontend/src/routes/results/+page.svelte
+++ frontend/src/routes/results/+page.svelte
@@ -41,7 +41,7 @@
             useCORS: true,
             scrollX: 0,
             scrollY: 0,
-            scale: 1,
+            scale: 4,
             removeContainer: true,
             windowHeight: 320,
             windowWidth: 368
