d16bd4a92c49fc7a768e295eb02448283e6bf8b6
Jordan <jordynwinnie@gmail.com>
2023-09-11 22:18:25 +0800

    Added Home page details

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,18 +2,21 @@
   import "../app.css";
 </script>
 
-<nav class="font-bebas flex justify-between p-2 bg-slate-200">
-  <p>🎶 Musical Mosaic</p>
-  <ul class="flex gap-2">
-    <li>Home</li>
-    <li>About</li>
-    <li>Privacy Policy</li>
-  </ul>
+<nav class="font-bebas p-2 bg-slate-200 text-3xl">
+  <div class="mx-8 flex justify-evenly">
+    <p >🎶 Musical Mosaic</p>
+    <ul class="flex gap-4">
+      <li> <a href="/">Home</a></li> 
+      <li> <a href="/about">About</a></li> 
+      <li> <a href="/privacy">Privacy Policy</a></li> 
+    </ul>
+  </div>
+  
   
 </nav>
 
-<slot />
+<slot class="container mx-auto"/>
 
-<footer class="flex justify-center">
+<footer class="flex justify-center font-bebas bg-slate-200 my-4">
   <p>Made with ❤ by  <a href="https://github.com/Jyodann/">Jyodann</a></p>
 </footer>
