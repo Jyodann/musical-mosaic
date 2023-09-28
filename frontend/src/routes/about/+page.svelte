48938d59ac692d99206db4b7851dbe177f42face
Jordan <jordynwinnie@gmail.com>
2023-09-28 21:24:35 +0800

    Updated About Page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,9 +1,23 @@
 <div class="flex flex-col gap-2 px-2 items-center">
     <p class="font-bebas text-4xl pt-2 text-center">About</p>
+    <p class="font-bold text-xl">Inspiration</p>
     <div class="md:w-[40rem] text-justify flex flex-col gap-4">
-        <p>Inspired by <a href="https://receiptify.herokuapp.com/index.html" class="underline">Receiptify</a>, Musical Mosaic is a service that creates a photo with albums that you listen to often</p>
-    </div>
+        <p>Musical Mosaic was inspired by <a href="https://receiptify.herokuapp.com/index.html" class="underline">Receiptify</a>, Musical Mosaic is a service that creates a photo with albums that you listen to often</p>
+    </div>   
+    <p class="font-bold text-xl">Contact</p>
+    <div class="md:w-[40rem] text-justify flex flex-col gap-4">
+        <p>Musical Mosaic was created by me, Jordan, I usually go by the alias <a class="underline" href="https://github.com/Jyodann/">Jyodann</a> online.</p>
+
+        <p>You may contact me through <a class="font-bold" href="mailto:jordynwinnie@gmail.com">Email</a> if you require any help, or would like to send feedback.</p>
+    </div>  
+
+    <p class="font-bold text-xl">Support me!</p>
+    <div class="md:w-[40rem] text-justify flex flex-col gap-4">
+        <p>I'm a hobbyist developer that likes to create random things for fun. If you particularly loved this, you may support me through donations. Remember to feed yourself first! Thank you!</p>
+        
+    </div> 
+    <a href='https://ko-fi.com/C0C633B3B' target='_blank' class="p-2">
+        <img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi3.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+
    
- 
-    
 </div>
